1046e74365e12bf69fdc30565620de420272abe2
crunnchyyy <someoneontheinternet17@gmail.com>
2026-01-31 17:51:33 +0000

    Added new_shoe and cut card object
    added the new_shoe methods which will call the new_deck method a number of times depending on how big the shoe size is and then shuffles the final card list.
    
    Additionally, added a cut card which will tell the game when to reshuffle the deck. To have this function in the game, I will have the program check if the value of a card ==12, in which case new_shoe will be called again.
    
    Next, I want to make a method called new_hand and then finally start working on the main game. I may also add another module called settings.py to control variables like the number of decks in a shoe

--- Card.py
+++ Card.py
@@ -1,3 +1,7 @@
+import random
+from types import new_class
+
+
 class Card:
     def __init__(self, rank, suit, value):
 
@@ -5,15 +9,27 @@ class Card:
         self.suit = suit
         self.value = value
 
-        print(f"Card rank: {self.rank}")
-        print(f"Card suit: {self.suit}")
-        print(f"Card value: {self.value}")
+        self.card_list = []
+        self.suit_list = ["Spades", "Hearts", "Clubs", "Diamonds"]
 
+    def new_shoe(self, decks):
+
+        for x in range(decks):
+            # Add full deck of cards to card_list
+            self.new_deck()
+
+        # Shuffle full shoe
+        random.shuffle(self.card_list)
+
+        # Get cut card location
+        cc_loc = int(len(self.card_list) * 0.75)
+        self.card_list.insert(cc_loc, Card("Cut Card", "None", 12))
+
+        for card in self.card_list:
+            print(f" {card.rank} of {card.suit}, Value: {card.value}")
 
     def new_deck(self):
 
-        self.card_list = []
-        self.suit_list = ["Spades", "Hearts", "Clubs", "Diamonds"]
         for y in range(4):
 
             # Decide suit by iterating through suit_list
@@ -31,10 +47,8 @@ class Card:
             self.card_list.append(Card("Queen", suit, 10))
             self.card_list.append(Card("King", suit, 10))
 
-            for card in self.card_list:
-                print(f" {card.rank} of {card.suit}, Value: {card.value}")
 
 instance = Card("Ace", "Spades", 11)
-instance.new_deck()
+instance.new_shoe(1)
 
 
