9d03ab365262942c90bed12f64c5bcd488caae29
crunnchyyy <someoneontheinternet17@gmail.com>
2026-01-31 17:24:50 +0000

    Added new_deck method to Card class
    For this commit, I wanted to figure out how to effectively create a list containing each card in a deck of cards without just making a 52 line method that adds them manually. I ended up with a solution that took 10 lines (without spacing and comments) which would make a full deck by iterating through a list of suits and then adding Card instances for an Ace, all the numbered cards using a for loop, and then the face cards.
    
    This method will be use multiple times in the next method i plan to make called new_shoe. In actual casinos, dealers use something called a "shoe" which is multiple decks shuffled together into one card holder. This is to make counting cards harder. I will implement this shoe to make the game more realistic

--- Card.py
+++ Card.py
@@ -1,7 +1,3 @@
-import random
-from types import new_class
-
-
 class Card:
     def __init__(self, rank, suit, value):
 
@@ -14,7 +10,31 @@ class Card:
         print(f"Card value: {self.value}")
 
 
+    def new_deck(self):
+
+        self.card_list = []
+        self.suit_list = ["Spades", "Hearts", "Clubs", "Diamonds"]
+        for y in range(4):
+
+            # Decide suit by iterating through suit_list
+            suit = self.suit_list[y]
+
+            # Add Ace card
+            self.card_list.append(Card("Ace", suit, 11))
+
+            # Add numbered cards
+            for x in range(9):
+                self.card_list.append(Card(str(x+2), suit, x+2))
+
+            # Add Face cards
+            self.card_list.append(Card("Jack", suit, 10))
+            self.card_list.append(Card("Queen", suit, 10))
+            self.card_list.append(Card("King", suit, 10))
+
+            for card in self.card_list:
+                print(f" {card.rank} of {card.suit}, Value: {card.value}")
 
 instance = Card("Ace", "Spades", 11)
+instance.new_deck()
 
 
